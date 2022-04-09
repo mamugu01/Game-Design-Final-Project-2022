33afe41f3a9bf5b53ea4566ea9c4dfb383fcbe67
juanchitognini <83304269+juanchitognini@users.noreply.github.com>
2022-04-09 19:06:15 -0400

    Play Again Button

---
+++ Mega Golf/Assets/Scripts/PlayAgain.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayAgain : MonoBehaviour
+{
+    public void Restart()
+    {
+        GlobalControl.Instance.strokes = 0;
+        SceneManager.LoadScene("scene1");
+    }
+}
