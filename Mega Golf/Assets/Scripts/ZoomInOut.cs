0e7bcf14035ae37f414c08d76f183d3a4eabe87f
Natinael13 <56659506+Natinael13@users.noreply.github.com>
2022-04-26 15:28:01 -0400

    Zoom In Zoom Out cap

--- Mega Golf/Assets/Scripts/ZoomInOut.cs
+++ Mega Golf/Assets/Scripts/ZoomInOut.cs
@@ -24,6 +24,7 @@ public class ZoomInOut : MonoBehaviour
         scrollData = Input.GetAxis("Mouse ScrollWheel");
 
         targetZoom -= scrollData * zoomFactor;
+        targetZoom = Mathf.Clamp(targetZoom, 4, 8f);
         cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime * zoomLerpSpeed);
     }
 }
